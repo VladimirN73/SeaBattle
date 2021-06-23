326a72358ce8a495032a5668ac05bb81d7c1b417
Vladimir Nalivaika <vladimir.nalivaika@bridging-it.de>
2021-06-23 17:57:05 +0200

    #1 Add/show info "buildAt" -- use german-time. Ooffset is hardcoded, so the summer time is active now. To be fixed later.

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -1,5 +1,5 @@
 export const base = {
     version: '0.1.3',
     name: "base",
-    buildAt: "dd.MM.yyyy hh:mm:ss",     // will be replaced during the build
+    buildAt: "23.06.2021 17:56:00",     // will be replaced during the build
   };
--- temp/temp.js
+++ temp/temp.js
@@ -1,39 +1,58 @@
 
-function func(){
-  var currentdate = new Date(); 
+function func()
+{
+  var currentdate = new Date();
+    
+  var utc_timestamp = Date.UTC(
+    currentdate.getUTCFullYear(),
+    currentdate.getUTCMonth(),
+    currentdate.getUTCDate() , 
+    currentdate.getUTCHours(),
+    currentdate.getUTCMinutes(), 
+    currentdate.getUTCSeconds(), 
+    currentdate.getUTCMilliseconds());
 
+  currentdate = new Date(utc_timestamp) ;
 
-  var day = currentdate.getDate() + "";
-  if (currentdate.getDate()<10) day = "0" + day;
+  currentdate.setHours(currentdate.getHours() + 2); // move to german-time
+
+  console.log("utc_timestamp", utc_timestamp.toString());
   
+  var strDateTime =  "" 
+    + addLeadingZero(currentdate.getUTCDate()) + "."  
+    + addLeadingZero(currentdate.getUTCMonth() + 1) + "." 
+    + addLeadingZero(currentdate.getUTCFullYear()) + " "  
+    + addLeadingZero(currentdate.getUTCHours()) + ":"  
+    + addLeadingZero(currentdate.getUTCMinutes()) + ":" 
+    + addLeadingZero(currentdate.getUTCSeconds());
 
-  var iMonth = currentdate.getMonth() + 1;
-  var month =  iMonth;
-  if (iMonth<10) month = "0" + month;
-  
+  console.log("strDateTime ", strDateTime);
 
+  var fs = require('fs')
+  var strFile ="./src/environments/environment.base.ts";
+  var strKey= "buildAt";
+  var regEx = /(buildAt)\s*(:).*(,)/g; 
 
-  var datetime = day + "." + month + "." + currentdate.getFullYear() + " "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-  console.log("build at " + datetime);
+  fs.readFile(strFile , 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    var result = data.replace(regEx, `${strKey}: "${strDateTime}",`);
 
-
-var fs = require('fs')
-var strFile ="./src/environments/environment.base.ts";
-var regEx = /(temp)\s*(:).*(,)/g;  // /\s+(temp)\s*(:)\s*(").*(")\s*(,)/g;  // /\s+(temp)\s*(:).*[,]\s*$/g; // /\s+(temp)\s*(:).*$/g
-
-fs.readFile(strFile , 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var result = data.replace(regEx, `temp: "${datetime}",`);
-
-  fs.writeFile(strFile , result, 'utf8', function (err) {
-     if (err) return console.log(err);
+    fs.writeFile(strFile , result, 'utf8', function (err) {
+      if (err) return console.log(err);
+    });
   });
-});
+}
+
+function addLeadingZero(str)
+{
+    var ret = str + "";
+    if (ret.length == 1)
+    {
+        ret = "0" + ret;
+    }
+    return ret;
 }
 
 func();
