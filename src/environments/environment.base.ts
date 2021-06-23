08c8e55845a757da43274f53634242a58e66c9aa
Vladimir Nalivaika <vladimir.nalivaika@bridging-it.de>
2021-06-23 17:04:33 +0200

    #1 Add/show info "buildAt" -- set the init (undefined) value

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -1,5 +1,5 @@
 export const base = {
     version: '0.1.3',
     name: "base",
-    buildAt: "23.06.2021 16:47:20",     // will be replaced during the build
+    buildAt: "dd.MM.yyyy hh:mm:ss",     // will be replaced during the build
   };
